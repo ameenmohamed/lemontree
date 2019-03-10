5ba8fceb3e59d4f432095d9b71c227f39e8d6bf2
ameenmohamed <ameen@hybridacumen.ie>
2019-03-10 12:44:45 +0000

    rainfix

--- lemontree.ino
+++ lemontree.ino
@@ -6,6 +6,7 @@ const int PUMP_PIN = 2;
 const int GOOD_MOISTURE = 70;
 const int LOW_SOIL_MOISTURE = 35;
 const int FAN_SPEED_MAX = 255;
+
 const String NO_VALUE = "NOVAL";
 
 String data = "";
@@ -24,86 +25,156 @@ int sfTime = 0; // sanfransisco local time in 24hrs
 int sunsetTime = 0;
 
 long  startTime =0;
+long  startTimeDrySoil =0;
 boolean timeStart = false;
-int showerSeconds = 5;
-int waitSeconds = 20;
+boolean timeStartDrySoil = false;
+
+
+int showerSecondsDrySoil = 5; // for rain
+int waitSecondsDrySoil = 10;  // wait delay for next shower 
+
+
+int showerSeconds = 5; // for rain
+int waitSeconds = 20;  // wait delay for next shower 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(FAN_PIN,OUTPUT);
+  pinMode(PUMP_PIN,OUTPUT);
+  
   pinMode(VAL_PROBE,INPUT);
+  pinMode(TEMP_PIN,INPUT);
   //sampledata="test<RT>remote temp12.12</RT><LT>localtemp10.10</LT><RL>remoteLux123</RL>,<WS>windSpeed</WS>,<RSM>remote soil moist</RSM>,<LSM>local soilMoist</LSM><RR> remote rain0</RR><LR>local riN</LR><RMST>1</RMST><LMST></LMST>";
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-    String incoming = ""; 
-    moisture = analogRead(VAL_PROBE);
-  //  Serial.print("Sensor:LocalSoilMoisture");Serial.print(moisture);Serial.println("------------------");
-    int tempSensor = analogRead(TEMP_PIN);
-    localTemp = readTemp(tempSensor);
- //   Serial.print("Sensor:LocalTemprature");Serial.print(tempSensor);Serial.println("------------------");
-
-    /// read all local sensor values before this line 
-    delay(2000);
-    if(Serial.available() > 0 ){
-      data = Serial.readString();
-      readData();
-      windSpeed = constrain(windSpeed,0,50);
-      moisture = map(moisture,550,10,0,100);
-
-  if(moisture < LOW_SOIL_MOISTURE ){
-    if(shouldStartPump()){
-     Serial.print(" plant says : water me !! ");
-     digitalWrite(PUMP_PIN,HIGH); Serial.println("Started pump .. ");
-    }
-    //trigger relay to start pump 
-    
-     
-  }
-  else if(moisture >= GOOD_MOISTURE){
-      // tigger relay to stop 
-     digitalWrite(PUMP_PIN,LOW);
-      Serial.print("Plant says : Stop Drowning me plss  !!!  ");
-  }
-//map(value, fromLow, fromHigh, toLow, toHigh) --- map(val, 0, 1023, 0, 255);
-    
-    fanSpeed = map(windSpeed,0,50,0,FAN_SPEED_MAX);
-    Serial.print("Fan Speed  ");Serial.print(fanSpeed);   Serial.println("  ");
- 
-    analogWrite(FAN_PIN,fanSpeed);
-    startLights();
-   
-}
-}
-boolean shouldStartPump(){
-  boolean retStatus = false;
-  // if raining 
-  if(isRaining == 1){ // if its raining 
-        if( !timeStart ){ // check if our shower duration counter has started
-             startTime = millis();// start the timer 
-             retStatus= true; 
-          }
-          else{    // if timer is already started 
-                long endTime = millis(); // get end time to see how much time passed 
-                long duration = endTime - startTime;
-                long endSecs = duration/1000;
-                if(endSecs >= showerSeconds){ // if current end time shows > than set shower secs 
-                    retStatus= false;        // stop the pump we elapsed allowed shower secs
+      void loop() {
+              
+              String incoming = ""; 
+              moisture = analogRead(VAL_PROBE);
+              //  Serial.print("Sensor:LocalSoilMoisture");Serial.print(moisture);Serial.println("------------------");
+              int tempSensor = analogRead(TEMP_PIN);
+              localTemp = readTemp(tempSensor);
+              //   Serial.print("Sensor:LocalTemprature");Serial.print(tempSensor);Serial.println("------------------");
+          
+              /// **************** . read all local sensor values before this line 
+              delay(4000); 
+             if(Serial.available() > 0 ){
+                data = Serial.readString(); 
                 }
-                else if(endSecs >= waitSeconds ){// now if local shower is stopped but its still raining there we still wait till elapsed time to save home from flooding
-                      startTime = millis(); // rset our base counter to current time  
-                      timeStart = false; // reset falg to default 
-                      retStatus =  true;
-               }
-            }
-         return retStatus;   
-    }
+             readData();
+             windSpeed = constrain(windSpeed,0,50);
+             //map(value, fromLow, fromHigh, toLow, toHigh) --- map(val, 0, 1023, 0, 255); - syntax for ref 
+            Serial.print("Moistureval: ");Serial.print(moisture); moisture = map(moisture,1023,0,0,100);Serial.print("Reset to: ");Serial.print(moisture);
 
-  
-    
-  }
+
+            //   section  ------------------------------------------------------------- SOIL Moisture START 
+            if(moisture < LOW_SOIL_MOISTURE ){
+              if(shouldStartPumpLowMoisture()){
+               Serial.print(" plant says : water me !! ");
+               digitalWrite(PUMP_PIN,HIGH); Serial.println("Starting  pump for Groot.. ");  //trigger relay to start pump 
+              }
+ 
+            }
+            else if(moisture >= GOOD_MOISTURE){
+                // tigger relay to stop 
+               digitalWrite(PUMP_PIN,LOW);
+                Serial.print("Plant says : Stop Drowning me plss  !!!  ");
+            }
+             // SOIL Moisture  section  --------------------------------------------------SOIL Moisture  END 
+
+             
+                
+            //   section  ------------------------------------------------------------- Wind  START 
+              
+              //map(value, fromLow, fromHigh, toLow, toHigh) --- map(val, 0, 1023, 0, 255); - syntax for ref 
+            fanSpeed = map(windSpeed,0,50,0,FAN_SPEED_MAX);
+            Serial.print("Fan Speed  ");Serial.print(fanSpeed);   Serial.println("  ");
+            analogWrite(FAN_PIN,fanSpeed);
+            
+            //   section  ------------------------------------------------------------- Wind  END 
+           
+         
+            //   section  ------------------------------------------------------------- Sunlight  START 
+            
+                       startLights();
+            
+            //   section  ------------------------------------------------------------- Sunlight  END  
+
+
+          //   section  ------------------------------------------------------------- Sound  START 
+            
+                       // code for sound 
+            
+            //   section  ------------------------------------------------------------- Sound  END 
+
+            //   section  ------------------------------------------------------------- HEAT   START 
+            
+                       // code for heat 
+            
+            //   section  ------------------------------------------------------------- HEAT   END 
+
+            //   section  ------------------------------------------------------------- RAIN   START 
+
+            if(isRaining == 1){
+              Serial.print(" its Raining there ...... !! ");
+               if(shouldStartPumpForRain()){
+               
+                  digitalWrite(PUMP_PIN,HIGH); Serial.println("Starting  pump for Rain.. ");  //trigger relay to start pump 
+              }else{
+                  digitalWrite(PUMP_PIN,LOW);
+                }
+            }//end rain if 
+            //   section  ------------------------------------------------------------- RAIN   END 
+            
+
+          } //end loop
+
+
+
+     boolean shouldStartPumpLowMoisture(){
+      boolean retStatus = false;
+              if( !timeStartDrySoil ){ // check if our shower duration counter has started
+                   startTimeDrySoil = millis();// start the timer 
+                   retStatus= true; 
+                }
+                else{    // if timer is already started 
+                      long endTime = millis(); // get end time to see how much time passed 
+                      long duration = endTime - startTimeDrySoil;
+                      long endSecs = duration/1000;
+                      if(endSecs >= showerSecondsDrySoil){ // if current end time shows > than set shower secs 
+                          retStatus= false;        // stop the pump we elapsed allowed shower secs
+                      }
+                      else if(endSecs >= waitSecondsDrySoil ){// now if local shower is stopped but its still raining there we still wait till elapsed time to save home from flooding
+                            startTimeDrySoil = millis(); // rset our base counter to current time  
+                            timeStartDrySoil = false; // reset falg to default 
+                            retStatus =  true;
+                     }
+                  }
+               return retStatus;   
+      } 
+          
+      boolean shouldStartPumpForRain(){
+        boolean retStatus = false;
+              if( !timeStart && (moisture < LOW_SOIL_MOISTURE)){ // check if our shower duration counter has started
+                   startTime = millis();// start the timer 
+                   retStatus= true; 
+                }
+                else{    // if timer is already started 
+                      long endTime = millis(); // get end time to see how much time passed 
+                      long duration = endTime - startTime;
+                      long endSecs = duration/1000;
+                      if(endSecs >= showerSeconds){ // if current end time shows > than set shower secs 
+                          retStatus= false;        // stop the pump we elapsed allowed shower secs
+                      }
+                      else if(endSecs >= waitSeconds ){// now if local shower is stopped but its still raining there we still wait till elapsed time to save home from flooding
+                            startTime = millis(); // rset our base counter to current time  
+                            timeStart = false; // reset falg to default 
+                            retStatus =  true;
+                     }
+                  }
+               return retStatus;   
+        }
 
 void readData(){
 //sampledata="test<RT>remote temp12.12</RT><LT>localtemp10.10</LT><RL>remoteLux123</RL>,<WS>windSpeed</WS>,<RSM>remote soil moist</RSM>,<LSM>local soilMoist</LSM><RR> remote rain0</RR><LR>local riN</LR><RMST>1</RMST><LMST></LMST><TM>14</TM>";
@@ -118,7 +189,7 @@ void readData(){
        if(!val.equalsIgnoreCase(NO_VALUE)){
            roomTemp = val.toDouble();
        }
-       Serial.print("Override:LocalTemprature:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(roomTemp);Serial.println("");
+   //    Serial.print("Override:LocalTemprature:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(roomTemp);Serial.println("");
  
 
       val = parseValue("<LSM>","</LSM>"); 
@@ -141,46 +212,46 @@ void parseCommonData(){
       if(!val.equalsIgnoreCase(NO_VALUE)){
            remTemp = val.toDouble();
       }
-      Serial.print("RemoteTemp:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(remTemp);Serial.println("");
+   //   Serial.print("RemoteTemp:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(remTemp);Serial.println("");
 
        val = parseValue("<RL>","</RL>");
        if(!val.equalsIgnoreCase(NO_VALUE)){
             remLux = val.toInt();
        }
-       Serial.print("RemoteLightValue:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(remLux);Serial.println("");
+  //     Serial.print("RemoteLightValue:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(remLux);Serial.println("");
       
       val = parseValue("<WS>","</WS>");
        if(!val.equalsIgnoreCase(NO_VALUE)){
           windSpeed = val.toDouble();
        }
-       Serial.print("Windspeed:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(windSpeed);Serial.println("");
+  //     Serial.print("Windspeed:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(windSpeed);Serial.println("");
 
 
       val = parseValue("<RSM>","</RSM>");
        if(!val.equalsIgnoreCase(NO_VALUE)){
            remSoilMoist = val.toInt();
        }
-       Serial.print("RemoteSoilMoisture:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(remSoilMoist);Serial.println("");
+  //     Serial.print("RemoteSoilMoisture:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(remSoilMoist);Serial.println("");
 
 
       val = parseValue("<RR>","</RR>");
        if(!val.equalsIgnoreCase(NO_VALUE)){
             isRaining = val.toInt();
        }
-       Serial.print("RemoteRainStatus:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(isRaining);Serial.println("");
+   //    Serial.print("RemoteRainStatus:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(isRaining);Serial.println("");
 
       
       val = parseValue("<TM>","</TM>");
       if(!val.equalsIgnoreCase(NO_VALUE)){
         sfTime = val.toInt();
       }
-      Serial.print("Timein24Hrs:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(sfTime);Serial.println("");
+  //    Serial.print("Timein24Hrs:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(sfTime);Serial.println("");
 
        val = parseValue("<SS>","</SS>");  
        if(!val.equalsIgnoreCase(NO_VALUE)){
            sunsetTime = val.toInt();
        }
-       Serial.print("SunSetTimein24Hrs:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(sunsetTime);Serial.println("");
+   //    Serial.print("SunSetTimein24Hrs:");Serial.print(val);Serial.print("-----CurrentVal:");Serial.print(sunsetTime);Serial.println("");
       
   }
 
@@ -221,13 +292,13 @@ float readTemp(int temp){
  void  startLights(){
       if(sfTime < 12){
          //blue lights on
-         Serial.print("Blue Lights on ...");
+         Serial.println("Blue Lights on ...");
         }
       else if(sfTime >12){
           // whitelights on
-          Serial.print("White Lights on ...");
+          Serial.println("White Lights on ...");
           }
       else if(sfTime > sunsetTime){
-          Serial.print("Red Lights on ...");
+          Serial.println("Red Lights on ...");
             }
     }
